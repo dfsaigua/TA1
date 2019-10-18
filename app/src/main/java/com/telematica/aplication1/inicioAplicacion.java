9497a51c6565df96234955d32a066c631283d737
Estudiante <Estudiante@GGIECTEMWRK223.espol.edu.ec>
2019-10-18 13:11:47 -0500

    Initial commit

---
+++ app/src/main/java/com/telematica/aplication1/inicioAplicacion.java
@@ -0,0 +1,13 @@
+package com.telematica.aplication1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inicioAplicacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_aplicacion);
+    }
+}
