660f303b4385ffb8325f3a5b9730e075180a1f18
julio.bodero <julio.bodero@easymetering.com>
2019-10-23 23:56:44 -0500

    Codigo Julio

--- app/src/main/java/com/telematica/aplication1/inicioAplicacion.java
+++ app/src/main/java/com/telematica/aplication1/inicioAplicacion.java
@@ -1,13 +1,29 @@
 package com.telematica.aplication1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
-public class inicioAplicacion extends AppCompatActivity {
+public class inicioAplicacion extends Activity  {
+    private final int DURACION_SPLASH = 3000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_inicio_aplicacion);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(inicioAplicacion.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
     }
 }
